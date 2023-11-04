b5cc1e67b6b89e1704289e5b77f53ab6b8e43051
Debasish <debapadhy12@gmail.com>
2023-11-04 10:53:55 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'counter';
+  count:number=0;
+  incr()
+  {
+    this.count++;
+  }
+  decr()
+  {
+    this.count--;
+  }
+  reset()
+  {
+    this.count=0;
+  }
+}
